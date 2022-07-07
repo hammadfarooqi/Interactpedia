1c116371b5670de32a78f371130844038fb39ed4
unknown <hammadfarooqi02@gmail.com>
2022-07-07 16:18:38 -0400

    Try grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {makeStyles} from "@material-ui/core/styles"
+import {Grid, Paper} from "@material-ui/core"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container>
+        <Grid item sm={6} md={6} lg={6}>
+          <p>Hj</p>
+        </Grid>
+        <Grid item sm={6} md={6} lg={6}>
+          <p>HJ</p>
+        </Grid>
+      </Grid>
     </div>
   );
 }
