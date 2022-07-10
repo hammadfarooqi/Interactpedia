48105be048ed3801da76e16fbcd476c18d70dcd4
unknown <hammadfarooqi02@gmail.com>
2022-07-10 12:50:58 -0400

    Auto display text from text file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import './App.css'
+import {useState } from "react"
 import {Grid} from "@mui/material"
 import TextDisplay from "./components/TextDisplay"
 import Question from "./components/Question"
 import Header from "./components/Header"
+import raw from "./article.txt"
 
 function App() {
+
+  const [parsed, setParsed] = useState([])
+
+  fetch(raw)
+  .then(r => r.text())
+  .then(text => {
+    setParsed(text.split("\n"));
+  });
+
   return (
     <div className="App">
       <Grid container padding={2} spacing = {5}>
@@ -13,7 +24,27 @@ function App() {
         </Grid>
         
         <Grid item sm={5} md={5} lg={5}>
-          <TextDisplay title="Text" />
+          <Grid container spacing = {2}>
+            {parsed.map((paragraph) => (
+
+              paragraph.includes(".") ?
+
+              <Grid item sm={12} md={12} lg={12}>
+                <TextDisplay 
+                  text={paragraph}
+                />
+              </Grid>
+
+              :
+
+              <Grid item sm={12} md={12} lg={12}>
+                <Header title={paragraph} />
+              </Grid>
+
+            )
+            )}
+
+          </Grid>
         </Grid>
         <Grid item sm={4} md={4} lg={4}>
           <Question title="Question" />
