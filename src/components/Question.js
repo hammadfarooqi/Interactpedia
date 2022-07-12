7d674e2ddd179077e303fd8baa8ee2a9fc4ed92c
Yechan <leyekkan@gmail.com>
2022-07-11 23:25:25 -0400

    hint

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,11 @@
-import { Card, FormControl, FormLabel, CardHeader, FormControlLabel, CardContent, Radio, RadioGroup, Grid, Button, Icon} from "@mui/material"
+import { Card, FormControl, FormLabel, CardHeader, FormControlLabel, CardContent, Radio, RadioGroup, Grid, Button, TextField} from "@mui/material"
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useState } from "react"
 import { red, green } from "@mui/material/colors";
+import Box from '@mui/material/Box';
+import { typography } from '@mui/system';
+
 
 
 
@@ -44,13 +47,13 @@ const Question = ({prompt, answers, correct, topic}) => {
               onChange = {(e)=>setCategory(e.target.value)}>
               <Grid container={0} align = "center" direction="column" justify="center">
                 <Grid item xs={6}>
-                  <FormControlLabel value="A" control={<Radio />} label="A" />
-                  <FormControlLabel value="C" control={<Radio />} label="C" />
+                  <FormControlLabel value="A" control={<Radio />} label={answers[0]} />
+                  <FormControlLabel value="C" control={<Radio />} label={answers[2]} />
                 </Grid>
 
                 <Grid item xs={6}>
-                  <FormControlLabel value="B" control={<Radio />} label="B" />
-                  <FormControlLabel value="D" control={<Radio />} label="D" />
+                  <FormControlLabel value="B" control={<Radio />} label={answers[1]} />
+                  <FormControlLabel value="D" control={<Radio />} label={answers[3]} />
                 </Grid>
               </Grid>
             </RadioGroup>
@@ -61,12 +64,12 @@ const Question = ({prompt, answers, correct, topic}) => {
               Submit
             </Button>
 
-
           </Grid>
         </CardContent>
       </Card>
       {isCorrect === 1 && <CheckCircleIcon sx={{ color: green[500], fontSize: 200 }}/>}
       {isCorrect === 0 && <CancelIcon sx={{ color: red[500], fontSize: 200 }}/>}
+      <Button variant="text" onClick =  {()=> Hint(topic)}>Hint</Button>
     </div>
   )
 }
