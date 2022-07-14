44061511d3fc7f6f258d1d5e139bf0db6592b70f
R1A2J <34625067+R1A2J@users.noreply.github.com>
2022-07-14 12:47:59 -0400

    Create questionsD.js

---
+++ src/questionsD.js
@@ -0,0 +1,65 @@
+export const qs = [
+  {
+    prompt: "With what technique can partial differential equations be solved in which all dimensions except one are represented?"
+    answers: ["A. 
+    correct: 'D',
+    topic: "Method of lines"
+    index: 0
+  },
+  {
+    prompt: "What have some integration routines been published as?"
+    answers:
+    correct: 'B',
+    topic: "Method of lines"
+    index: 1
+  },
+  {
+    prompt: "What does the method of lines most often refer to?"
+    answers:
+    correct: 'D'
+    topic: "Method of lines"
+    index: 2
+  },
+  {
+    prompt: "Which algorithm is used to solve certain differential equations using spectral methods?"
+    answers: 
+    correct: 'D'
+    topic: "Spectral method"
+    index: 3
+  },
+  {
+    prompt: "What are two areas in which spectral methods are used?"
+    answers:
+    correct: 'C'
+    topic: "Spectral method"
+    index: 4
+  },
+  {
+    prompt: "Spectral methods and which other type of method are closely related to each other?"
+    answers: 
+    correct: 'D'
+    topic: "Spectral method"
+    index: 5
+  },
+  {
+    prompt: "Which of these groups of methods include only overlapping domain decomposition methods?"
+    answers:
+    correct: 'A'
+    topic: "Domain decomposition methods"
+    index: 6
+  },
+  {
+    prompt: "What happens to subdomains in non-overlapping domain decomposition methods?"
+    answers: 
+    correct: 'B'
+    topic: "Domain decomposition methods"
+    index: 7
+  },
+  {
+    prompt: "What is another method of non-overlapping domain decomposition methods?"
+    answers: 
+    correct: 'C'
+    topic: "Domain decomposition methods"
+    index: 8
+  }
+]
