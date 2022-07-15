1f217164ec40cfcacdd07a0a92fc5084a18dd1c1
Solano71 <108429369+Solano71@users.noreply.github.com>
2022-07-14 21:03:21 -0400

    Create questionsC.js

---
+++ src/questionsC.js
@@ -0,0 +1,67 @@
+export const qs = [
+    {
+        prompt: "How are eggs preserved for oological studies?",
+        answers: ["A. A tiny hole is made in the egg and its contents are extracted ", "B. The eggs are frozen to induce homeostasis", "C. They are labeled for organization", "D. The eggs are embalmed to reduce moisture loss"],
+        correct: 'A',
+        topic: "Collections",
+        index: 0
+    },
+    {
+        prompt: "Which of these is NOT a beneficial contribution of Museums to bird species studies?",
+        answers: ["A. Collections of eggs", "B. Samples used for pesticide studies", "C. The invention of the blow drill", "D. Taxonomic studies with bird collections"],
+        correct: 'C',
+        topic: "Collections",
+        index: 1
+
+    },
+    {
+        prompt: "What makes freeze drying less reliable for Morphometrics?",
+        answers: ["A. The storage of specimens in spirit is required to freeze dry", "B. Blood and DNA samples decay over time", "C. Freeze drying ruins the taxonomic structure of the bird skin", "D. Stomach and anatomy contents shrink over time"],
+        correct: 'D',
+        topic: "Collections",
+        index: 2
+
+    },
+    {
+        prompt: "Which of these is a technique for capturing birds?",
+        answers: ["A. Bal-chatri", "B. Spotting scopes", "C. Spotting scopes", "D. Ectoparasites"],
+        correct: 'A',
+        topic: "In the Field",
+        index: 3
+    },
+    {
+        prompt: "The sex of a bird can be determined by __________",
+        answers: ["A. Arthropod-borne viruses", "B. Skull ossification", "C. Examination of anatomy in non dimorphic species", "D. Bird liming"],
+        correct: 'C',
+        topic: "In the Field",
+        index: 4
+    },
+    {
+        prompt: "________ has been found to be a useful tool for the detection/documentation of elusive species?",
+        answers: ["A. Camera traps", "B. Frugivory", "C. Demographic studies", "D. Zoonoses"],
+        correct: 'A',
+        topic: "In the Field",
+        index: 5
+    },
+    {
+        prompt: "What is an example of a nondestructive DNA sample of a bird?",
+        answers: ["A. Mass spectrometric analysis of hydrogen isotopes", "B. Bird skins", "C. Collecting bird eggs", "D. Experimenting on young bird species"],
+        correct: 'A',
+        topic: "In the Laboratory",
+        index: 6
+    },
+    {
+        prompt: "What studies of birds are predominantly lab based?",
+        answers: ["A. Bird societal systems", "B. Mating rituals", "C. Migratory patterns", "D. Bird intelligence/song learning"],
+        correct: 'D',
+        topic: "In the Laboratory",
+        index: 7
+    },
+    {
+        prompt: "The use of ______ allows for the study of the effect of adding or silencing a specific gene?",
+        answers: ["A. Dopamine receptor D4", "B. Speciation", "C. Electroporation", "D. Emlen Funnel"],
+        correct: 'C',
+        topic: "In the Laboratory",
+        index: 8
+    }
+]
